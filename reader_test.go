1d9d864804fe18490c2fbc1bd4cfe1814efa75a8
Ilia Choly <icholy@accipiterradar.com>
2020-04-22 16:26:00 -0400

    add test for strict mode

--- reader_test.go
+++ reader_test.go
@@ -9,7 +9,7 @@ import (
 	"gotest.tools/v3/golden"
 )
 
-func TestDecodeMap(t *testing.T) {
+func TestReader(t *testing.T) {
 	schema := Schema{
 		Fields: map[string]Type{
 			"number":      NumberType,
@@ -25,4 +25,23 @@ func TestDecodeMap(t *testing.T) {
 	data, err := ioutil.ReadAll(r)
 	assert.NilError(t, err)
 	golden.Assert(t, string(data), "example.json.golden")
+}
+
+func TestReaderStrict(t *testing.T) {
+	schema := Schema{
+		Strict: true,
+		Fields: map[string]Type{
+			"number":      NumberType,
+			"omg":         BoolType,
+			"size":        NumberType,
+			"temperature": NumberType,
+		},
+	}
+	f, err := os.Open(golden.Path("example.log"))
+	assert.NilError(t, err)
+	defer f.Close()
+	r := NewReader(f, schema)
+	data, err := ioutil.ReadAll(r)
+	assert.NilError(t, err)
+	golden.Assert(t, string(data), "example.json.strict.golden")
 }
